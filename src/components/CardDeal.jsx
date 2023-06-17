f7ea226abbbbda67b919f62380ede5a002e3d373
shivangbirla <birlashivang01@gmail.com>
2023-06-17 14:59:40 +0530

    react-page

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Credit cards are safer than cash, offer stronger fraud protection than
+        debit, provide rewards without altering spending habits, simplify
+        expense tracking, and are one of the fastest ways to build credit
+        responsibly.
+      </p>
+      <Button styles="mt-10" />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
